8fe6dad0e5461bbee494872c4edcd47867f2907a
JustinLung <justinlung77@gmail.com>
2022-11-20 09:28:49 +0100

    global styling added and layout made

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	// import { Footer, Header } from 'ilojo-bar-components/components';
+	import Header from 'ilojo-bar-components/components/Header.svelte';
+	import Footer from 'ilojo-bar-components/components/Footer.svelte';
+	import '../styles/style.css';
+</script>
+
+<Header />
+<main>
+	<slot />
+</main>
+<Footer companyName="Legacy" />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import WindowSection from 'ilojo-bar-components/components/WindowSection.svelte';
+	import Window from 'ilojo-bar-components/components/Window.svelte';
+</script>
+
+<WindowSection>
+	<Window />
+</WindowSection>
