224d998c012f9d009064cba10e62c8ae2a7fe890
JustinLung <justinlung77@gmail.com>
2022-11-21 11:04:13 +0100

    data fetched

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,19 @@
 <script lang="ts">
-	import WindowSection from 'ilojo-bar-components/components/WindowSection.svelte';
 	import Window from 'ilojo-bar-components/components/Window.svelte';
+	import WindowSection from 'ilojo-bar-components/components/WindowSection.svelte';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+	console.log(data);
+	const pageTitle = 'Stories';
 </script>
 
 <WindowSection>
-	<Window />
+	{#each data.stories as story}
+		<Window
+			storyTitle={story.title}
+			storyImage={story.thumbnail.url}
+			altTag={story.thumbnail.title}
+		/>
+	{/each}
 </WindowSection>
