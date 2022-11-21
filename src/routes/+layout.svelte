e5a77d9c39f94d03b8e0acc934a715f0f2dbc729
JustinLung <justinlung77@gmail.com>
2022-11-21 11:04:17 +0100

    data fetched

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	// import { Footer, Header } from 'ilojo-bar-components/components';
 	import Header from 'ilojo-bar-components/components/Header.svelte';
 	import Footer from 'ilojo-bar-components/components/Footer.svelte';
 	import '../styles/style.css';
